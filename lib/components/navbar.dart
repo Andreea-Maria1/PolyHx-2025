005cfc3fbf2b612786018407932199b0d72921ba
Andreea Maria <andreeamariaghioltan@gmail.com>
2025-02-08 16:11:47 -0500

    Added Navbar icons

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
-class Navbar extends StatelessWidget {
-  const Navbar({
+class Navbar extends StatefulWidget {
+  Navbar({
     super.key,
   });
 
   @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return NavigationBar(destinations: const [
-      NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-      NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
-    ], selectedIndex: 0);
+    return NavigationBar(
+      destinations: const [
+        NavigationDestination(icon: Icon(Icons.waves), label: 'Inondation'),
+        NavigationDestination(icon: Icon(Icons.landslide), label: 'Glissement'),
+        NavigationDestination(icon: Icon(Icons.forest), label: 'Réserves'),
+      ],
+      onDestinationSelected: (int index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      selectedIndex: selectedIndex,
+    );
   }
 }
--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -45,7 +45,7 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      bottomNavigationBar: const Navbar(),
+      bottomNavigationBar: Navbar(),
     );
   }
 }
